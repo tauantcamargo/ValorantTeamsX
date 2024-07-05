b6937a6a89d3d20d95c01cb78a94d08d94c8b757
Tauan Camargo <lukyhenson@gmail.com>
2024-07-05 20:17:26 -0300

    feat: add onboardDetail component

---
+++ ValorantTeamsX/Views/Onboarding/OnboardDetailView.swift
@@ -0,0 +1,64 @@
+//
+//  SwiftUIView.swift
+//  ValorantTeamsX
+//
+//  Created by Tauan Tathiell Camargo on 05/07/24.
+//
+
+import SwiftUI
+
+struct OnboardDetailView: View {
+    var color: Color
+    var title: String
+    var subtitle: String?
+    var buttonLabel: String?
+    var buttonAction: () -> Void
+    
+    var body: some View {
+        ZStack {
+            Color(color)
+            
+            VStack {
+                Spacer()
+                Spacer()
+                
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 250)
+                
+                VStack (spacing: 4) {
+                    Text(title)
+                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        .font(Font.system(size: 20))
+                        .bold()
+                    if let subtitle = subtitle {
+                        Text(subtitle)
+                            .multilineTextAlignment(.center)
+                            .font(Font.system(size: 16))
+                            .bold()
+                            .foregroundStyle(.indigo)
+                    }
+                }
+                .padding(.horizontal, 40)
+                
+                Spacer()
+                
+                Button {
+                    buttonAction()
+                } label: {
+                    Text(buttonLabel ?? "Continue")
+                }
+                
+                Spacer()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    OnboardDetailView(color: .mint, title: "Hey Valorant Champion", subtitle: "Are you ready to discover all details about your favorite teams?", buttonLabel: "Of course!", buttonAction: {
+        print("Hello")
+    } )
+}
