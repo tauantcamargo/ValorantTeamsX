f9ca336ef339c2f44f388fc908036b2121c42f72
Tauan Camargo <lukyhenson@gmail.com>
2024-07-05 18:14:54 -0300

    Initial Commit

---
+++ ValorantTeamsX/ValorantTeamsXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantTeamsXApp.swift
+//  ValorantTeamsX
+//
+//  Created by Tauan Tathiell Camargo on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ValorantTeamsXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
